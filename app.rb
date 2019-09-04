27aace2c2cc2145d2bc300ce9dc8e132220af989
Mike <mjwalshe@gmail.com>
2019-09-04 16:57:04 +0100

    Configure session

--- app.rb
+++ app.rb
@@ -1,15 +1,27 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/play' do
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
+  post '/names' do
+    session[:Player1] = params[:Player1]
+    session[:Player2] = params[:Player2]
+    redirect to('/play')
+  end
+
+  get '/play' do
+    @player1 = session[:Player1]
+    @player2 = session[:Player2]
     erb(:play)
   end
 
+
   run! if app_file == $0
 end
+
+# erb(:play)
