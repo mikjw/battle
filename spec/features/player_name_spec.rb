8875e25d393354f5d3a36091c798557a8d283d23
Mike <mjwalshe@gmail.com>
2019-09-05 14:04:45 +0100

    Add web helpers spec

--- spec/features/player_name_spec.rb
+++ spec/features/player_name_spec.rb
@@ -1,9 +1,6 @@
 feature "names form" do
   scenario "expect player names" do
-    visit('/')
-    fill_in('Player1', with: 'James')
-    fill_in('Player2', with: 'Mike')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Player: James - hit points: 10 Player: Mike - hit points: 10')
   end
 end
---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,6 @@
+def sign_in_and_play
+  visit('/')
+  fill_in('Player1', with: 'James')
+  fill_in('Player2', with: 'Mike')
+  click_button('Submit')
+end 
